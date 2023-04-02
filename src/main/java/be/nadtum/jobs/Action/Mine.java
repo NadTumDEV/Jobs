dacaee413a7b2ab9e2222e865fb3b893a58e69c0
NadTum <98037911+NadTumDEV@users.noreply.github.com>
2023-04-02 23:01:41 +0200

    update mine action check itemstack exist

--- src/main/java/be/nadtum/jobs/Action/Mine.java
+++ src/main/java/be/nadtum/jobs/Action/Mine.java
@@ -10,8 +10,11 @@ import org.bukkit.event.block.BlockBreakEvent;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Random;
 
 public class Mine implements Listener {
+
+    private Random random;
 
     @EventHandler
     public void PlayerBreakBlock(BlockBreakEvent event) throws SQLException {
@@ -22,11 +25,19 @@ public class Mine implements Listener {
 
         Statement stmt = ConnectionBuilder.getConnection().createStatement();
         ResultSet result = stmt.executeQuery("SELECT * FROM `PROFIL` WHERE `UUID` = '" + player.getUniqueId() + "'");
+
+        //create a profil player if not exist
         if(!result.next()){
             stmt.executeUpdate("INSERT INTO `PROFIL`(`UUID`) " + "VALUES ('" + event.getPlayer().getUniqueId() + "')");
         }
 
-
+        //You must check that the block is present in the database
+        result = stmt.executeQuery("SELECT * FROM `DEPOT` WHERE `ITEMSTACK` = '" + block.getType() + "'");
+        if(!result.next()){
+            result.close();
+            stmt.close();
+            return;
+        }
 
         result.close();
         stmt.close();
